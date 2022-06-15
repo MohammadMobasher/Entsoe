66265407948a12d271c9a27ed826376c121dbd90
mohammad <m.mobasher.z@gmail.com>
2022-06-15 15:12:04 +0430

    init DocumentType

---
+++ Entsoe/Models/DocumentType.cs
@@ -0,0 +1,171 @@
+﻿
+namespace Entsoe.Models
+{
+    public enum DocumentType
+    {
+        /// <summary>
+        /// Finalised schedule
+        /// </summary>
+        A09,
+        /// <summary>
+        /// Aggregated energy data report
+        /// </summary>
+        A11,
+        /// <summary>
+        /// Acquiring system operator reserve schedule
+        /// </summary>
+        A15,
+        /// <summary>
+        /// Bid document
+        /// </summary>
+        A24,
+        /// <summary>
+        /// Allocation result document
+        /// </summary>
+        A25,
+        /// <summary>
+        /// Capacity document
+        /// </summary>
+        A26,
+        /// <summary>
+        /// Agreed capacity
+        /// </summary>
+        A31,
+        /// <summary>
+        /// Reserve allocation result document
+        /// </summary>
+        A38,
+        /// <summary>
+        /// Price Document
+        /// </summary>
+        A44,
+        /// <summary>
+        /// Estimated Net Transfer Capacity
+        /// </summary>
+        A61,
+        /// <summary>
+        /// Redispatch notice
+        /// </summary>
+        A63,
+        /// <summary>
+        /// System total load
+        /// </summary>
+        A65,
+        /// <summary>
+        /// Installed generation per type
+        /// </summary>
+        A68,
+        /// <summary>
+        /// Wind and solar forecast
+        /// </summary>
+        A69,
+        /// <summary>
+        /// Load forecast margin
+        /// </summary>
+        A70,
+        /// <summary>
+        /// Generation forecast
+        /// </summary>
+        A71,
+        /// <summary>
+        /// Reservoir filling information
+        /// </summary>
+        A72,
+        /// <summary>
+        /// Actual generation
+        /// </summary>
+        A73,
+        /// <summary>
+        /// Wind and solar generation
+        /// </summary>
+        A74,
+        /// <summary>
+        /// /Actual generation per type
+        /// </summary>
+        A75,
+        /// <summary>
+        /// Load unavailability
+        /// </summary>
+        A76,
+        /// <summary>
+        /// Production unavailability
+        /// </summary>
+        A77,
+        /// <summary>
+        ///  Transmission unavailability
+        /// </summary>
+        A78,
+        /// <summary>
+        /// Offshore grid infrastructure unavailability
+        /// </summary>
+        A79,
+        /// <summary>
+        /// Generation unavailability
+        /// </summary>
+        A80,
+        /// <summary>
+        /// Contracted reserves
+        /// </summary>
+        A81,
+        /// <summary>
+        /// Accepted offers
+        /// </summary>
+        A82,
+        /// <summary>
+        /// Activated balancing quantities
+        /// </summary>
+        A83,
+        /// <summary>
+        /// Activated balancing prices
+        /// </summary>
+        A84,
+        /// <summary>
+        /// Imbalance prices
+        /// </summary>
+        A85,
+        /// <summary>
+        /// Imbalance volume
+        /// </summary>
+        A86,
+        /// <summary>
+        /// Financial situation
+        /// </summary>
+        A87,
+        /// <summary>
+        /// Cross border balancing
+        /// </summary>
+        A88,
+        /// <summary>
+        /// Contracted reserve prices
+        /// </summary>
+        A89,
+        /// <summary>
+        /// Interconnection network expansion
+        /// </summary>
+        A90,
+        /// <summary>
+        /// Counter trade notice
+        /// </summary>
+        A91,
+        /// <summary>
+        /// Congestion costs
+        /// </summary>
+        A92,
+        /// <summary>
+        /// DC link capacity
+        /// </summary>
+        A93,
+        /// <summary>
+        /// Non EU allocations
+        /// </summary>
+        A94,
+        /// <summary>
+        /// Configuration document
+        /// </summary>
+        A95,
+        /// <summary>
+        /// Flow-based allocations
+        /// </summary>
+        B11
+    }
+}
