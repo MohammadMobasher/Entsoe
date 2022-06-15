199cfa20d642f8de4d92202a7035498fdd1bdc52
mohammad <m.mobasher.z@gmail.com>
2022-06-15 15:10:45 +0430

    init MarketAgreementType

---
+++ Entsoe/Models/MarketAgreementType.cs
@@ -0,0 +1,39 @@
+﻿
+namespace Entsoe.Models
+{
+    public enum MarketAgreementType
+    {
+        /// <summary>
+        /// Daily
+        /// </summary>
+        A01,
+        /// <summary>
+        /// Weekly
+        /// </summary>
+        A02,
+        /// <summary>
+        /// Monthly
+        /// </summary>
+        A03,
+        /// <summary>
+        /// Yearly
+        /// </summary>
+        A04,
+        /// <summary>
+        /// Total
+        /// </summary>
+        A05,
+        /// <summary>
+        /// Long term
+        /// </summary>
+        A06,
+        /// <summary>
+        /// Intraday
+        /// </summary>
+        A07,
+        /// <summary>
+        /// Hourly
+        /// </summary>
+        A13
+    }
+}
