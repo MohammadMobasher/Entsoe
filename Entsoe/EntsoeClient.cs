f4a85da58b1f7bfba8bce7f595bd639d154609df
mohammad <m.mobasher.z@gmail.com>
2022-06-15 14:12:57 +0430

    first commit

---
+++ Entsoe/EntsoeClient.cs
@@ -0,0 +1,34 @@
+﻿using RestSharp;
+
+namespace Entsoe
+{
+    /// <summary>
+    ///  Client to perform API calls and return the raw responses API-documentation:
+    /// https://transparency.entsoe.eu/content/static_content/Static%20content/web%20api/Guide.html#_request_methods
+    /// Attributions: Parts of the code for parsing Entsoe responses were copied
+    /// from https://github.com/tmrowco/electricitymap
+    /// </summary>
+    public class EntsoeClient
+    {
+        private readonly RestClient _client;
+        private readonly string _apiKey;
+        private const string _entsoeUrl = "https://transparency.entsoe.eu/api";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="apiKey"></param>
+        public EntsoeClient(string apiKey)
+        {
+            _apiKey = apiKey;
+            var options = new RestClientOptions(_entsoeUrl) 
+            { 
+                Timeout = 30000
+            };
+            _client = new RestClient(options);
+        }
+
+
+
+    }
+}
---
+++ Entsoe/Model/Area.cs
@@ -0,0 +1,56 @@
+﻿
+namespace Entsoe.Model
+{
+    public static class Area
+    {
+        private static readonly List<AreaItem> AreaItems = new()
+        {
+            new AreaItem
+            {
+                CountryCode = "NL",
+                Domain = "10YNL----------L",
+                CountryName = "Netherlands, TenneT NL BZ / CA/ MBA",
+                TimeZoneName = "Europe/Amsterdam"
+            },
+
+            new AreaItem
+            {
+                CountryCode = "BE",
+                Domain = "10YBE----------2",
+                CountryName = "Belgium, Elia BZ / CA / MBA",
+                TimeZoneName = "Europe/Brussels"
+            }
+        };
+
+
+        /// <summary>
+        /// get an area base on countryCode
+        /// </summary>
+        /// <param name="countryCode">like NL</param>
+        /// <returns></returns>
+        public static AreaItem? GetArea(string countryCode) => AreaItems.SingleOrDefault(x => x.CountryCode.ToLower() == countryCode.ToLower());
+
+    }
+
+
+    public class AreaItem
+    {
+
+        public string CountryCode { get; set; }
+        public string Domain { get; set; }
+        public string CountryName { get; set; }
+
+        public string TimeZoneName { get; set; }
+        public TimeZoneInfo TimeZoneInfo
+        {
+            get
+            {
+                if (!string.IsNullOrEmpty(TimeZoneName))
+                {
+                    return TimeZoneInfo.FindSystemTimeZoneById(TimeZoneName);
+                }
+                throw new Exception($"There is no TimeZoneInfo {TimeZoneName}");
+            }
+        }
+    }
+}
