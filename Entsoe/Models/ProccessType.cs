f6407e1fe38bcd741d302dc4d023665cc2865759
mohammad <m.mobasher.z@gmail.com>
2022-06-16 13:04:39 +0430

    add new enum => ProccessType

---
+++ Entsoe/Models/ProccessType.cs
@@ -0,0 +1,63 @@
+﻿
+namespace Entsoe.Models
+{
+    public enum ProccessType
+    {
+        /// <summary>
+        /// Day ahead
+        /// </summary>
+        A01,
+        /// <summary>
+        /// Intra day incremental
+        /// </summary>
+        A02,
+        /// <summary>
+        /// Realised
+        /// </summary>
+        A16,
+        /// <summary>
+        /// Intraday total
+        /// </summary>
+        A18,
+        /// <summary>
+        /// Week ahead
+        /// </summary>
+        A31,
+        /// <summary>
+        /// Month ahead
+        /// </summary>
+        A32,
+        /// <summary>
+        /// Year ahead
+        /// </summary>
+        A33,
+        /// <summary>
+        /// Synchronisation process
+        /// </summary>
+        A39,
+        /// <summary>
+        /// Intraday process
+        /// </summary>
+        A40,
+        /// <summary>
+        /// Replacement reserve
+        /// </summary>
+        A46,
+        /// <summary>
+        /// Manual frequency restoration reserve
+        /// </summary>
+        A47,
+        /// <summary>
+        /// Automatic frequency restoration reserve
+        /// </summary>
+        A51,
+        /// <summary>
+        /// Frequency containment reserve
+        /// </summary>
+        A52,
+        /// <summary>
+        /// Frequency restoration reserve
+        /// </summary>
+        A56
+    }
+}
