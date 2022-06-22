483511dfda28598b9d5c533d9dec77e2f25ef77b
mohammad <m.mobasher.z@gmail.com>
2022-06-22 19:50:40 +0430

    inir PstType ssot

---
+++ Entsoe/Models/PsrType.cs
@@ -0,0 +1,115 @@
+﻿
+namespace Entsoe.Models
+{
+    public enum PsrType
+    {
+        /// <summary>
+        /// Mixed
+        /// </summary>
+        A03,
+        /// <summary>
+        /// Generation
+        /// </summary>
+        A04,
+        /// <summary>
+        /// Load
+        /// </summary>
+        A05,
+        /// <summary>
+        /// Biomass
+        /// </summary>
+        B01,
+        /// <summary>
+        /// Fossil Brown coal/Lignite
+        /// </summary>
+        B02,
+        /// <summary>
+        /// Fossil Coal-derived gas
+        /// </summary>
+        B03,
+        /// <summary>
+        /// Fossil Gas
+        /// </summary>
+        B04,
+        /// <summary>
+        /// Fossil Hard coal
+        /// </summary>
+        B05,
+        /// <summary>
+        /// Fossil Oil
+        /// </summary>
+        B06,
+        /// <summary>
+        /// Fossil Oil shale
+        /// </summary>
+        B07,
+        /// <summary>
+        /// Fossil Peat
+        /// </summary>
+        B08,
+        /// <summary>
+        /// Geothermal
+        /// </summary>
+        B09,
+        /// <summary>
+        /// Hydro Pumped Storage
+        /// </summary>
+        B010,
+        /// <summary>
+        /// Hydro Run-of-river and poundage
+        /// </summary>
+        B011,
+        /// <summary>
+        /// Hydro Water Reservoir
+        /// </summary>
+        B012,
+        /// <summary>
+        /// Marine
+        /// </summary>
+        B013,
+        /// <summary>
+        /// Nuclear
+        /// </summary>
+        B014,
+        /// <summary>
+        /// Other renewable
+        /// </summary>
+        B015,
+        /// <summary>
+        /// Solar
+        /// </summary>
+        B016,
+        /// <summary>
+        /// Waste
+        /// </summary>
+        B017,
+        /// <summary>
+        /// Wind Offshore
+        /// </summary>
+        B018,
+        /// <summary>
+        /// Wind Onshore
+        /// </summary>
+        B019,
+        /// <summary>
+        /// Other
+        /// </summary>
+        B020,
+        /// <summary>
+        /// AC Link
+        /// </summary>
+        B021,
+        /// <summary>
+        /// DC Link
+        /// </summary>
+        B022,
+        /// <summary>
+        /// Substation
+        /// </summary>
+        B023,
+        /// <summary>
+        /// Transformer
+        /// </summary>
+        B024,
+    }
+}
