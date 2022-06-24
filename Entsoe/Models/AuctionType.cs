8b0554b033a85a090fdc1544189459d686492f46
mohammad <m.mobasher.z@gmail.com>
2022-06-24 11:20:50 +0430

    add AcuctionType

---
+++ Entsoe/Models/AuctionType.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Entsoe.Models
+{
+    public enum AuctionType
+    {
+        /// <summary>
+        /// Implicit
+        /// </summary>
+        A01,
+        /// <summary>
+        /// Explicit
+        /// </summary>
+        A02,
+    }
+}
