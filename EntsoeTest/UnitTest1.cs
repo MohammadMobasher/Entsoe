89ff18fc341efa117094b691233c1af5af796e2e
mohammad <m.mobasher.z@gmail.com>
2022-06-24 11:32:33 +0430

    init test project

---
+++ EntsoeTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EntsoeTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
